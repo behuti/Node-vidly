2960322eafc4313ae09645cfb01e70b630b09229
Eduardo Henao <eduardo.henao@splicedigital.com>
2021-01-05 20:15:30 -0500

    put n' delete genre endpoints added

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 // Validate functions
 function validateGenre(genre) {
 	const schema = Joi.object({
-		name: Joi.string().min(3).required()
+		name: Joi.string().min(3).required(),
 	});
 
 	return schema.validate(genre);
@@ -46,6 +46,7 @@ app.get("/api/genres/:id", (req, res) => {
 	res.send(genre);
 });
 
+// Post new Genre
 app.post("/api/genres", (req, res) => {
 	const { error } = validateGenre(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
@@ -58,6 +59,35 @@ app.post("/api/genres", (req, res) => {
 	genres.push(genre);
 	res.send(genre);
 });
+
+app.put("/api/genres/:id", (req, res) => {
+	// Look for the id of the genre
+	const genre = genres.find((g) => g.id === parseInt(req.params.id));
+	if (!genre) return res.status(404).send("The genre with the given id does not exists.");
+
+	// Validate the body of the request
+	const { error } = validateGenre(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	// Modify the genre
+	console.log(genre.name);
+	genre.name = req.body.name;
+
+	res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+	// Find the Genre by id
+	const genre = genres.find( (g) => g.id === parseInt(req.params.id) );
+	if (!genre) return res.status(404).send("The Genre with the given id does not exists.");
+
+	// Delete Genre
+	const index = genres.indexOf(genre);
+	genres.splice(index, 1);
+
+	//Return 
+	res.send(genre);
+})
 
 app.listen(3000, () => {
 	console.log(`Listening on port ${3000}...`);
