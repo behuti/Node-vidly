a85a6bbb5d28ea95cf270f2a5690fe186d1ed16a
Eduardo Henao <eduardo.henao@splicedigital.com>
2021-01-06 09:42:56 -0500

    Config added

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+
+const config = require('config');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require("joi");
+const logger = require('./logger');
 const express = require("express");
 const app = express();
 
+//Middlewares
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'))
+app.use(helmet());
+
+//Configuration
+console.log(`Application Name: ${config.get('name')}`)
+console.log(`Mail Server: ${config.get('mail.host')}`)
+
+// Detect if the environment is development and run Morgan only for that case
+if (app.get('env') === 'development') { 
+	app.use(morgan('tiny'));
+	console.log('Morgan enabled...')
+}
+
+app.use(logger.log);
+app.use(logger.authenticate);
 
 const genres = [
 	{
